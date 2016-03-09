8472190678b8d73b2ab59423c6a348414c9709e7
HuyHL <hpit.vie@gmail.com>
2016-03-10 00:17:01 +0700

    Add ?completed paramater

---
+++ variable-playground.js
@@ -0,0 +1,23 @@
+/*
+var person = {
+    name : 'Huy',
+    age: 28
+};
+
+function updatePerson(obj) {
+    obj.age = 20;
+}
+
+updatePerson(person);
+
+console.log(person);*/
+
+var grades = [15,88];
+
+function addGrade(gradesArray) {
+    gradesArray.push(15);
+    debugger;
+}
+
+addGrade(grades);
+console.log(grades);
